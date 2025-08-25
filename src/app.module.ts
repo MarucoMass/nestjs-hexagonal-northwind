cb23f5b9f27736e7cfbfe5cdafbd291080515661
MarucoMass <mariomass23@gmail.com>
2025-08-24 22:11:51 -0300

    entities created and folder structure started

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { CoreModule } from './core/core.module';
+import { InfraestructureModule } from './infraestructure/infraestructure.module';
+
+@Module({
+  imports: [CoreModule, InfraestructureModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
